66def03d0348cad4ec15b5bc9263f98146d7c6f6
Sarah Mayoral-Osmolski <sarah@semolski.com>
2021-11-15 10:06:35 -0600

    Created functional component, setting up Weather API

---
+++ src/components/WeatherGrid.jsx
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from "react";
+
+import {DataGrid} from "@material-ui/data-grid";
+
+const WEATHER_API_KEY = "ebcbb378d5995d489f96ae39ca154495";
+const WEATHER_API_URL = ``
+
+const WeatherGrid = () => {
+
+
+    return
+        <div>
+
+        </div>
+
+}
+
+export default WeatherGrid;
