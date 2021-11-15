03be588c5e4f047f6585510ec6686f41033e57bd
Sarah Mayoral-Osmolski <sarah@semolski.com>
2021-11-15 10:30:09 -0600

    Added useState and useEffect hooks for fetching data

--- src/components/WeatherGrid.jsx
+++ src/components/WeatherGrid.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from "react";
 import {DataGrid, GridColDef} from "@material-ui/data-grid";
 
 const WEATHER_API_KEY = "ebcbb378d5995d489f96ae39ca154495";
-const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/find?lat=51.89&lon=0.89&cnt=20&appid=${WEATHER_API_KEY}`
+const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/find?lat=51.89&lon=0.89&cnt=20&&units=imperial&appid=${WEATHER_API_KEY}`
 
 const columns = [
     {
@@ -31,12 +31,26 @@ const columns = [
 
 const WeatherGrid = () => {
 
+    const [weatherData, setWeatherData] = useState([]);
+
+    useEffect(() => {
+        fetch(WEATHER_API_URL)
+            .then((res) => res.json())
+            .then((data) => {
+                // console.log(data.list)
+                setWeatherData(data.list)
+            })
+    }, [])
+
+
     return <div>
+        <div style={{height:700, width: '100%'}}>
             <DataGrid
                 rows={rows}
                 column={columns}
                 pageSize={20}
             />
+        </div>
         </div>
 
 }
