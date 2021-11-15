cc8925b4e2ebb6b760ce945205f52860302216bf
Sarah Mayoral-Osmolski <sarah@semolski.com>
2021-11-15 11:19:11 -0600

    Implemented valueGetter to fetch nested data
    Added GridColDef[] MUI attribute to gain access to valueGetter

--- src/components/WeatherGrid.jsx
+++ src/components/WeatherGrid.jsx
@@ -3,31 +3,63 @@ import React, {useState, useEffect} from "react";
 import {DataGrid, GridColDef} from "@material-ui/data-grid";
 
 const WEATHER_API_KEY = "ebcbb378d5995d489f96ae39ca154495";
-const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/find?lat=51.89&lon=0.89&cnt=20&&units=imperial&appid=${WEATHER_API_KEY}`
+const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/find?lat=51.89&lon=0.89&cnt=20&&units=imperial&appid=${WEATHER_API_KEY}`;
 
-const columns = [
+const columns: GridColDef[] = [
     {
         field: 'name',
         headerName: 'City',
-        width:150,
+        width: 150,
     },
     {
         field: 'temp',
         headerName: 'Temperature',
-        width: 170
+        width: 170,
+        valueGetter: (params) => {
+            let result = [];
+            if (params.row.main) {
+                if (params.row.main.temp) {
+                    result.push( params.row.main.temp );
+                }
+            } else {
+                result = ["Unknown"];
+            }
+            return result;
+        }
     },
     {
         field: 'humidity',
         headerName: 'Humidity',
-        width: '150'
+        width: 150,
+        valueGetter: (params) => {
+            let result = [];
+            if (params.row.main) {
+                if (params.row.main.humidity) {
+                    result.push( params.row.main.humidity );
+                }
+            } else {
+                result = ["Unknown"];
+            }
+            return result;
+        }
     },
     {
         field: 'pressure',
-        headerName: 'Pressurre',
-        width: '150'
-    }
+        headerName: 'Pressure',
+        width: 150,
+        valueGetter: (params) => {
+            let result = [];
+            if (params.row.main) {
+                if (params.row.main.pressure) {
+                    result.push( params.row.main.pressure );
+                }
+            } else {
+                result = ["Unknown"];
+            }
+            return result;
+        }
+    },
 ];
-
 
 const WeatherGrid = () => {
 
@@ -38,21 +70,22 @@ const WeatherGrid = () => {
             .then((res) => res.json())
             .then((data) => {
                 // console.log(data.list)
-                setWeatherData(data.list)
-            })
-    }, [])
+                setWeatherData(data.list);
+            });
 
+    }, []);
 
     return <div>
-        <div style={{height:700, width: '100%'}}>
+        <div style= {{height: 700, width: '100%'}}>
             <DataGrid
-                rows={rows}
-                column={columns}
+                rows={weatherData}
+                columns={columns}
                 pageSize={20}
             />
         </div>
-        </div>
+    </div>;
 
-}
+};
 
 export default WeatherGrid;
+
